529e949b6912d8623d6dbeae9e0d7f178a2b30f1
Marcos <gut17909@uvg.edu.gt>
2022-03-17 13:21:37 -0600

    update

--- 03-counter-app/src/Tests/PrimeraApp.test.js
+++ 03-counter-app/src/Tests/PrimeraApp.test.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import {shallow} from 'enzyme'
 import PrimeraApp from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp/>>', () => {
+    {/*
     test('debe de mostrar el mensaje "Hola soy Marcos"', () => { 
         const saludo = 'Hola soy Marcos';
         const {getByText} = render(<PrimeraApp saludo={saludo}/>)
         expect(getByText(saludo)).toBeInTheDocument();
-     });
+     }); */}
+    
+    test('debe de mostrar <PrimeraApp/> correctamente', () => { 
+        const saludo = 'Hola soy Marcos'
+        const wrapper = shallow(<PrimeraApp saludo={saludo}/>)
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el subtituo enviado por props', () => { 
+        const saludo = 'Hola soy Marcos'
+        const sub = 'Soy un subtitulo'
+        const wrapper = shallow(
+            <PrimeraApp 
+                saludo={saludo} 
+                subtitulo={sub}
+            />
+        );
+        const textoParrafo = wrapper.find('p').text();
+        expect(textoParrafo).toBe(sub);
+
+       });
 });
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp'
-//import CounterApp from './CounterApp'
+//import PrimeraApp from './PrimeraApp'
+import CounterApp from './CounterApp'
 import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(<PrimeraApp saludo='Hola soy Marcos' />, divRoot)//
-//ReactDOM.render(<CounterApp value={10} />, divRoot)
+//ReactDOM.render(<PrimeraApp saludo='Hola soy Marcos' />, divRoot)//
+ReactDOM.render(<CounterApp value={10} />, divRoot)
